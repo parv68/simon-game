107c3508f3a1d4190e31f8b207ac7c315d7a667c
parv <147272703+parv68@users.noreply.github.com>
2024-02-14 15:04:44 +0530

    Update game.js

--- game.js
+++ game.js
@@ -67,10 +67,6 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
 
 function startOver() {
   level = 0;
